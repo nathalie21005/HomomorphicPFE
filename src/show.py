692be3c3d1e99dcde57d1d72dc9e14cdd5a28c75
nathalie21005 <nathaliewehbe@hotmail.com>
2015-12-13 14:04:53 +0200

    faceeuclideandistance.py and  LabelsforDistance.py

--- src/show.py
+++ src/show.py
@@ -10,7 +10,6 @@ import matplotlib.cm as cm # color map
 import pylab
 
 
-
 #print mean_face
 
 #E=eigenfaces.split('\n')
@@ -26,27 +25,27 @@ nb = raw_input()
 #nb="1223"
 
 with open ("../faces/faceR") as faceR:
-	w=[]
-	for v in faceR:
-		if v.split()[0]!=nb:
-			pass
-		else:
-			w=[float(x) for x in v.split()[1:]]
-			break  
-	
-	
-	if v.split()[0] == nb:  
-		print "PCA reconstructed ..."
-		f = plt.figure()
-		f.add_subplot(121)
-		i = M.transpose().dot(w) + 	mean_face.transpose()
-		plt.imshow(i.reshape(128,128), cmap = cm.Greys_r)
-		#pylab.show()
-		
-		print "original ..."
-		f.add_subplot(122)
-		orig = np.fromfile("../faces/rawdata/rawdata/"+nb, dtype=np.uint8)		
-		#print len(orig)
-		#print 128*128  
-		plt.imshow(orig.reshape(128,128), cmap = cm.Greys_r)
-		pylab.show() 
+    w=[]
+    for v in faceR:
+        if v.split()[0]!=nb:
+            pass
+        else:
+            w=[float(x) for x in v.split()[1:]]
+            break  
+    
+    
+    if v.split()[0] == nb:  
+        print "PCA reconstructed ..."
+        f = plt.figure()
+        f.add_subplot(121)
+        i = M.transpose().dot(w) +     mean_face.transpose()
+        plt.imshow(i.reshape(128,128), cmap = cm.Greys_r)  # @UndefinedVariable
+        #pylab.show()
+        
+        print "original ..."
+        f.add_subplot(122)
+        orig = np.fromfile("../faces/rawdata/rawdata/"+nb, dtype=np.uint8)        
+        #print len(orig)
+        #print 128*128  
+        plt.imshow(orig.reshape(128,128), cmap = cm.Greys_r)
+        pylab.show() 
